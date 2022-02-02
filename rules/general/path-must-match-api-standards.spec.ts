5957f0e8a9b1468aa4be83e8cfaf8fc5b0d37258
Mark Brockhoff <mark.brockhoff@mail.schwarz>
2022-02-02 09:36:38 +0100

    Added two example tests

--- rules/general/path-must-match-api-standards.spec.ts
+++ rules/general/path-must-match-api-standards.spec.ts
@@ -1,61 +1,40 @@
 import s from "@stoplight/spectral-core";
+import { IRange } from "@stoplight/types/dist/parsers";
 import ruleset from "./path-must-match-api-standards.yml";
 
 describe("path-must-match-api-standards", () => {
-  it("lints", async () => {
+  it("has a correct path", async () => {
     const spectral = new s.Spectral();
-    spectral.setRuleset(ruleset)
-    const result = await spectral.run(`
-      {
-        "paths": {
-          "/api-linting/api/v1/rules": {
-              "get": {
-                  "operationId": "RulesController_getCompanyApiRules",
-                  "summary": "",
-                  "description": "Get company API linting rules for Spectral as YAML.",
-                  "parameters": [
-                      {
-                          "name": "apiType",
-                          "required": true,
-                          "in": "query",
-                          "allowEmptyValue": false,
-                          "schema": {
-                              "enum": [
-                                  "backend_for_frontend",
-                                  "legacy_api",
-                                  "product_api"
-                              ],
-                              "type": "string"
-                          }
-                      }
-                  ],
-                  "responses": {
-                      "201": {
-                          "description": "Provides company API linting rules as stream download."
-                      },
-                      "400": {
-                          "description": "\"apiType\" query param is missing."
-                      },
-                      "401": {
-                          "description": "API call was not authenticated"
-                      }
-                  },
-                  "security": [
-                      {
-                          "basic": []
-                      }
-                  ],
-                  "tags": [
-                      "rules",
-                  ]
-              }
-          }
-        }
-      }
-    `);
+    spectral.setRuleset(ruleset);
+    const result = await spectral.run(getTestSpec("/api-linting/api/v1/rules"));
 
-    console.log(result);
-    
-    expect(result).toHaveLength(1);
+    expect(result).toHaveLength(0);
   });
+
+  it("fails if the version is missing", async () => {
+    const spectral = new s.Spectral();
+    spectral.setRuleset(ruleset);
+    const result = await spectral.run(getTestSpec("/api-linting/api/rules"));
+
+    expect(result).toHaveLength(1);
+    expect(result[0].code).toEqual("path-must-match-api-standards");
+    expect(result[0].range).toEqual<IRange>({
+      start: {
+        line: 3,
+        character: 33,
+      },
+      end: {
+        line: 3,
+        character: 36,
+      },
+    });
+  });
+
+  const getTestSpec = (path: string) => `
+    {
+      "paths": {
+        "${path}": {}
+      }
+    }
+    `;
 });
