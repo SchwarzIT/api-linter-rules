bff5b6ab9895b41b46bd39f6c1cf8603e0acedf2
Mark Brockhoff <mark.brockhoff@mail.schwarz>
2022-05-23 17:18:08 +0200

    Formatted setup spectral

--- util/setup-spectral.ts
+++ util/setup-spectral.ts
@@ -1,11 +1,12 @@
 import { Spectral } from "@stoplight/spectral-core";
-import {bundleAndLoadRuleset} from "@stoplight/spectral-ruleset-bundler/dist/loader/node";
+import { bundleAndLoadRuleset } from "@stoplight/spectral-ruleset-bundler/dist/loader/node";
 import path from "path";
 import fs from "fs";
-import {fetch} from "@stoplight/spectral-runtime";
+import { fetch } from "@stoplight/spectral-runtime";
 
 export async function setupSpectral(rulesetFile: string) {
-    const spectral = new Spectral();
-    spectral.setRuleset(await bundleAndLoadRuleset(path.resolve(rulesetFile), {fs, fetch}));
-    return spectral;
+  const spectral = new Spectral();
+  const ruleset = await bundleAndLoadRuleset(path.resolve(rulesetFile), { fs, fetch });
+  spectral.setRuleset(ruleset);
+  return spectral;
 }
