367ac99d9b6f1b4912b9e77ff959293daeb67fe7
Mark Brockhoff <mark.brockhoff@mail.schwarz>
2022-05-30 09:26:51 +0200

    Added test for oas2-must-have-response-body

--- rules/endpoint/must-have-response-body.spec.ts
+++ rules/endpoint/must-have-response-body.spec.ts
@@ -4,10 +4,11 @@ import { setupSpectral } from "../../util/setup-spectral";
 
 const getTestSpec = (
   httpCodes: { 200?: unknown; 201?: unknown },
-  fields: { content?: unknown; description?: unknown },
-  path: string
+  fields: { content?: unknown; description?: unknown } | { schema?: unknown; description?: unknown },
+  path: string,
+  version: string = "3.0.0"
 ) => ({
-  openapi: "3.0.0",
+  [version.startsWith("3") ? "openapi" : "swagger"]: version,
   paths: {
     [path]: {
       get: {
@@ -19,15 +20,17 @@ const getTestSpec = (
 
 const getTestSpecJson = (
   httpCodes: { 200?: unknown; 201?: unknown },
-  fields: { content?: unknown; description?: unknown },
-  path: string
-): string => JSON.stringify(getTestSpec(httpCodes, fields, path), null, 2);
+  fields: { content?: unknown; schema?: unknown; description?: unknown },
+  path: string,
+  version?: string
+): string => JSON.stringify(getTestSpec(httpCodes, fields, path, version), null, 2);
 
 const getTestSpecYaml = (
   httpCodes: { 200?: unknown; 201?: unknown },
-  fields: { content?: unknown; description?: unknown },
-  path: string
-): string => YAML.stringify(getTestSpec(httpCodes, fields, path), null, 2);
+  fields: { content?: unknown; schema?: unknown; description?: unknown },
+  path: string,
+  version?: string
+): string => YAML.stringify(getTestSpec(httpCodes, fields, path, version), null, 2);
 
 describe.each([getTestSpecJson, getTestSpecYaml])("must-have-response-body - %p", (getTestSpec) => {
   let spectral: Spectral;
@@ -55,6 +58,37 @@ describe.each([getTestSpecJson, getTestSpecYaml])("must-have-response-body - %p"
       } else {
         expect(result.length).toBeGreaterThanOrEqual(1);
         expect(result[0].code).toEqual("must-have-response-body");
+      }
+    }
+  );
+});
+
+describe.each([getTestSpecJson, getTestSpecYaml])("oas2-must-have-response-body - %p", (getTestSpec) => {
+  let spectral: Spectral;
+
+  beforeEach(async () => {
+    spectral = await setupSpectral("rules/endpoint/must-have-response-body.yml");
+  });
+
+  test.each<[boolean, ...Parameters<typeof getTestSpec>]>([
+    [true, { 200: true, 201: true }, { schema: true, description: true }, "/api/some/path"],
+    [false, { 200: true, 201: true }, { schema: true }, "/api/some/path"],
+    [false, { 200: true, 201: true }, { description: true }, "/api/some/path"],
+    [false, { 200: true, 201: true }, {}, "/api/some/path"],
+    [true, { 200: true }, { schema: true, description: true }, "/api/some/path"],
+    [true, { 201: true }, { schema: true, description: true }, "/api/some/path"],
+    [true, { 200: true, 201: true }, {}, "/well-known/some/path"],
+  ])(
+    "%s for the http codes %o and response content %o for path %p",
+    async (expectedResult, httpCodes, fields, path) => {
+      const testSpec = getTestSpec(httpCodes, fields, path, "2.0");
+      const result = await spectral.run(testSpec);
+
+      if (expectedResult) {
+        expect(result).toHaveLength(0);
+      } else {
+        expect(result.length).toBeGreaterThanOrEqual(1);
+        expect(result[0].code).toEqual("oas2-must-have-response-body");
       }
     }
   );
