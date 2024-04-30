7ed3c754fb7e9e41821382964e5b6945efd4c483
Lucas Maciuga <lucas@maciuga.de>
2024-04-30 11:23:45 +0200

    added new test for "path-must-specify-tags" and fixed the jsonpath selector to not fetch none "paths" paths

--- rules/endpoint/path-must-specify-tags.spec.ts
+++ rules/endpoint/path-must-specify-tags.spec.ts
@@ -25,6 +25,11 @@ describe("path-must-specify-tags", () => {
     expect(result[0].code).toEqual("path-must-specify-tags");
   });
 
+  it("do not fail if the keywords for http methods are used in components", async () => {
+    const result = await spectral.run(getComponentTestSpec());
+    expect(result).toHaveLength(0);
+  });
+
   it("ignores paths under the well-known route", async () => {
     const result = await spectral.run(getTestSpec(undefined, "/well-known/health"));
     expect(result).toHaveLength(0);
@@ -60,4 +65,115 @@ describe("path-must-specify-tags", () => {
         },
       },
     });
+
+const getComponentTestSpec = (tags?: string[], path = "/api/some/path") =>
+  JSON.stringify({
+    "openapi": "3.0.2",
+    "info": {
+      "title": "Sometest",
+      "version": "0.1",
+      "description": "This example shows that options will give a false positive in the schema ShowMe an false positive warning",
+      "contact": {
+        "email": "lucas@maciuga.de",
+        "name": "Lucas Maciuga",
+        "url": "https://have.none"
+      }
+    },
+    "servers": [
+      {
+        "url": "https://test.api.schwarz"
+      }
+    ],
+    "tags": [
+      {
+        "name": "SomeTag",
+        "description": "some description"
+      }
+    ],
+    "paths": {
+      "/twin/api/v1/somethings": {
+        "get": {
+          "operationId": "GetSomething",
+          "description": "hi test",
+          "tags": [
+            "SomeTag"
+          ],
+          "responses": {
+            "200": {
+              "description": "OK",
+              "content": {
+                "application/json": {
+                  "schema": {
+                    "$ref": "#/components/schemas/ShowMe"
+                  }
+                }
+              }
+            },
+            "401": {
+              "description": "Unauthorized"
+            }
+          }
+        }
+      }
+    },
+    "components": {
+      "schemas": {
+        "Option": {
+          "type": "object",
+          "properties": {
+            "field1": {
+              "type": "string"
+            }
+          },
+          "example": {
+            "field1": "val1"
+          }
+        },
+        "ShowMe": {
+          "type": "object",
+          "properties": {
+            "input": {
+              "type": "object",
+              "properties": {
+                "get": {
+                  "type": "string"
+                },
+                "delete": {
+                  "type": "boolean"
+                },
+                "options": {
+                  "type": "array",
+                  "items": {
+                    "$ref": "#/components/schemas/Option"
+                  },
+                  "nullable": true
+                }
+              }
+            }
+          },
+          "example": {
+            "input": {
+              "options": [
+                {
+                  "field1": "val1"
+                },
+                {
+                  "field1": "val2"
+                },
+                {
+                  "field1": "val3"
+                },
+                {
+                  "field1": "val4"
+                },
+                {
+                  "field1": "val5"
+                }
+              ]
+            }
+          }
+        }
+      }
+    }
+  });
 });
