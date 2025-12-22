f683a7a6fdc47d70aebb1b8914a04396a37a8c2b
Mark Brockhoff <mark.brockhoff@mail.schwarz>
2025-12-22 11:10:44 +0100

    feat: refactor to monorepo & add first rule

---
+++ packages/openapi/src/rules/documentation/info-description.spec.ts
@@ -0,0 +1,40 @@
+import { beforeAll, describe, expect, it } from 'vitest';
+import { Spectral } from '@stoplight/spectral-core';
+import { infoDescription } from './info-description';
+
+describe('info-description', () => {
+  let spectral: Spectral;
+
+  beforeAll(() => {
+    spectral = new Spectral();
+    spectral.setRuleset({ rules: { 'info-description': infoDescription } });
+  });
+
+  it('has no errors', async () => {
+    const result = await spectral.run({
+      info: {
+        description: '#'.repeat(100),
+      },
+    });
+
+    expect(result).toHaveLength(0);
+  });
+
+  it('fails no description is provided', async () => {
+    const result = await spectral.run({ info: {} });
+
+    expect(result).toHaveLength(1);
+    expect(result[0]?.code).toEqual('info-description');
+  });
+
+  it('fails no description is less than 100 characters long', async () => {
+    const result = await spectral.run({
+      info: {
+        description: '#'.repeat(99),
+      },
+    });
+
+    expect(result).toHaveLength(1);
+    expect(result[0]?.code).toEqual('info-description');
+  });
+});
