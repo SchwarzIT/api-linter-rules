48c2f3a02600cc38b14a933e58ac4613b9908e3d
Mark Brockhoff <mark.brockhoff@mail.schwarz>
2025-12-23 09:03:06 +0100

    chore: migrate rule "contact-information"

---
+++ packages/openapi/src/rules/documentation/contact-information.ts
@@ -0,0 +1,23 @@
+import type { RuleDefinition } from '@stoplight/spectral-core';
+import { truthy } from '@stoplight/spectral-functions';
+
+export const contactInformation: RuleDefinition = {
+  description: 'Every API must have a contact containing name, email and a url',
+  message: '{{description}}; property {{property}} is missing',
+  severity: 'error',
+  given: '$.info.contact',
+  then: [
+    {
+      field: 'name',
+      function: truthy,
+    },
+    {
+      field: 'email',
+      function: truthy,
+    },
+    {
+      field: 'url',
+      function: truthy,
+    },
+  ],
+};
