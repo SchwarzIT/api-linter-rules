01666fc48b258fb39d0e0044c533806057b894a1
Mark Brockhoff <mark.brockhoff@mail.schwarz>
2025-12-23 09:25:38 +0100

    chore: migrate endpoint rules

---
+++ packages/openapi/src/rules/endpoint/must-define-example-schema.ts
@@ -0,0 +1,17 @@
+import type { RuleDefinition } from '@stoplight/spectral-core';
+import { xor } from '@stoplight/spectral-functions';
+
+export const mustDefineExampleSchema: RuleDefinition = {
+  description: 'Every DTO must define at least one example',
+  message: '{{description}}; DTO is lacking an example {{path}}',
+  severity: 'error',
+  given: '$.components.schemas.*',
+  then: [
+    {
+      function: xor,
+      functionOptions: {
+        properties: ['example', 'examples'],
+      },
+    },
+  ],
+};
---
+++ packages/openapi/src/rules/endpoint/no-http-verbs-in-resources.spec.ts
@@ -0,0 +1,54 @@
+import { Spectral } from '@stoplight/spectral-core';
+import { beforeAll, describe, expect, it } from 'vitest';
+import { noHttpVerbsInResources } from './no-http-verbs-in-resources';
+
+describe('no-http-verbs-in-resources', () => {
+  let spectral: Spectral;
+
+  beforeAll(async () => {
+    spectral = new Spectral();
+    spectral.setRuleset({
+      rules: { 'no-http-verbs-in-resources': noHttpVerbsInResources },
+    });
+  });
+
+  it('has no errors if no http verb is used inside the path', async () => {
+    const result = await spectral.run(getTestSpec('/api/test'));
+    expect(result).toHaveLength(0);
+  });
+
+  it.each([
+    '/get/',
+    '/get',
+    '/api/get',
+    '/api/get/test',
+    '/post/',
+    '/post',
+    '/api/post',
+    '/api/post/test',
+    '/put/',
+    '/put',
+    '/api/put',
+    '/api/put/test',
+    '/patch/',
+    '/patch',
+    '/api/patch',
+    '/api/patch/test',
+    '/delete/',
+    '/delete',
+    '/api/delete',
+    '/api/delete/test',
+  ])('fails if the path %s is used', async (path) => {
+    const result = await spectral.run(getTestSpec(path));
+    expect(result).toHaveLength(1);
+    expect(result[0]?.code).toEqual('no-http-verbs-in-resources');
+  });
+
+  const getTestSpec = (path: string) => {
+    return {
+      paths: {
+        [path]: true,
+      },
+    };
+  };
+});
